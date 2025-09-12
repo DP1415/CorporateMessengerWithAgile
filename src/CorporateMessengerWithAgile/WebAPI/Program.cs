ee2ca1e474a788d580d97074e903bf1fba70bc36
DP1415 <Daniil-2005-zima@mail.ru>
2025-09-12 21:39:50 +0300

    Первичная настройка

---
+++ src/CorporateMessengerWithAgile/WebAPI/Program.cs
@@ -0,0 +1,43 @@
+using Application;
+using FluentValidation;
+using System.Reflection;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // настройка
+            builder.Services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(DependencyInjection.Assembly));
+            builder.Services.AddValidatorsFromAssembly(DependencyInjection.Assembly);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
