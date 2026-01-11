03890906c7cd5fe4beba4042ef3644bf8be2a160
dp1415 <Daniil-2005-zima@mail.ru>
2026-01-11 23:06:04 +0300

    Добавлена возможность авторизоваться через сваггер

--- src/CorporateMessengerWithAgile/ReactApp/ReactApp.Server/Program.cs
+++ src/CorporateMessengerWithAgile/ReactApp/ReactApp.Server/Program.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 namespace ReactApp.Server
 {
@@ -12,13 +13,27 @@ namespace ReactApp.Server
             builder.Services.AddControllers();
             builder.Services.AddAuth(builder.Configuration);
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
             builder.Services.AddAutoMapper(Application.DependencyInjection.Assembly);
             builder.Services.AddMediatR(cnf => cnf.RegisterServicesFromAssemblies(Application.DependencyInjection.Assembly));
             builder.Services.AddValidatorsFromAssembly(Application.DependencyInjection.Assembly);
             builder.Services.AddDbContext<Persistence.AppDbContext>(options => options.UseInMemoryDatabase("CorporateMessengerDb"));
-
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                opt.AddSecurityRequirement((document) => new OpenApiSecurityRequirement()
+                {
+                    [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+                });
+            });
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowFrontend", policy =>
