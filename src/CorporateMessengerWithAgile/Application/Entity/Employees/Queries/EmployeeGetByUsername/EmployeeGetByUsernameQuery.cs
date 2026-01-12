186eb2ea67afbcc0b2081f8b7191b3200b4545a1
dp1415 <Daniil-2005-zima@mail.ru>
2026-01-12 22:47:39 +0300

    Добавление команды получения данных сотрудника через username

---
+++ src/CorporateMessengerWithAgile/Application/Entity/Employees/Queries/EmployeeGetByUsername/EmployeeGetByUsernameQuery.cs
@@ -0,0 +1,18 @@
+using Application.AbsQuery;
+using Application.AbsQuery.Options;
+using Application.Dto;
+using Domain.Entity;
+
+namespace Application.Entity.Employees.Queries.EmployeeGetByUsername
+{
+    public record EmployeeGetByUsernameQuery(string Username)
+        : AbsQueryEntityWithOptions<Employee, EmployeeWithCompanyAndPositionDto>(
+            [
+                new Filter<Employee>(e => e.User != null && e.User.Username.Value == Username),
+                new Include<Employee, Company>(e => e.Company),
+                new Include<Employee, PositionInCompany>(e => e.PositionInCompany),
+                new Include<Employee, User>(e => e.User),
+                new Include<Employee, ICollection<TeamMember>>(e => e.TeamMembers)
+            ]
+        );
+}
---
+++ src/CorporateMessengerWithAgile/Application/Entity/Employees/Queries/EmployeeGetByUsername/EmployeeGetByUsernameQueryHandler.cs
@@ -0,0 +1,15 @@
+using Application.AbsQuery;
+using Application.Dto;
+using AutoMapper;
+using Domain.Entity;
+using Persistence;
+
+namespace Application.Entity.Employees.Queries.EmployeeGetByUsername
+{
+    public class EmployeeGetByUsernameQueryHandler(AppDbContext context, IMapper mapper)
+        : AbsQueryEntityWithOptionsHandler<
+            EmployeeGetByUsernameQuery,
+            Employee,
+            EmployeeWithCompanyAndPositionDto
+            >(context, mapper);
+}
