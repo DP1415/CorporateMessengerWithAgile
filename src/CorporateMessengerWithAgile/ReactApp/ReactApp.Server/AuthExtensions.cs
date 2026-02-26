bf7dea2a9dd016675647b5a28bef8b7c1ee2c517
dp1415 <Daniil-2005-zima@mail.ru>
2026-02-26 21:41:52 +0300

    Мелкие правки в AuthExtensions

--- src/CorporateMessengerWithAgile/ReactApp/ReactApp.Server/AuthExtensions.cs
+++ src/CorporateMessengerWithAgile/ReactApp/ReactApp.Server/AuthExtensions.cs
@@ -8,34 +8,33 @@ namespace ReactApp.Server
         public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
         {
             var jwtSettings = configuration.GetSection("JwtSettings");
-            var secretKey = jwtSettings["SecretKey"] ?? throw new InvalidOperationException("Jwt:SecretKey is missing in appsettings.json");
+            var secretKey = jwtSettings["SecretKey"] ?? throw new InvalidOperationException("JwtSettings:SecretKey is missing in appsettings.json");
+            var issuer = jwtSettings["Issuer"] ?? throw new InvalidOperationException("JwtSettings:Issuer is missing in appsettings.json");
+            var audience = jwtSettings["Audience"] ?? throw new InvalidOperationException("JwtSettings:Audience is missing in appsettings.json");
+
+            TokenValidationParameters tokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey)),
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                ClockSkew = TimeSpan.Zero
+            };
 
             services
                 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey)),
-                        ValidIssuer = jwtSettings["Issuer"],
-                        ValidAudience = jwtSettings["Audience"],
-                        ClockSkew = TimeSpan.Zero
-                    };
-                });
+                .AddJwtBearer(options => { options.TokenValidationParameters = tokenValidationParameters; });
 
             services.AddScoped<Application.IJwtProvider, Infrastructure.Authentication.JwtProvider>();
             services.Configure<Infrastructure.Authentication.JwtProviderSettings>(jwtSettings);
 
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
-                options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
-                options.AddPolicy("UserOrAdmin", policy => policy.RequireRole("User", "Admin"));
-            });
+            //services.AddAuthorizationBuilder()
+            //    .AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"))
+            //    .AddPolicy("UserOnly", policy => policy.RequireRole("User"))
+            //    .AddPolicy("UserOrAdmin", policy => policy.RequireRole("User", "Admin"));
 
             return services;
         }
